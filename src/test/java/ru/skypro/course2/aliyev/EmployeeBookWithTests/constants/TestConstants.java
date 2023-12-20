4ee8d3528dfda56500e924905a592fc29e4c4090
VGAliyev <v.aliev84@yandex.ru>
2023-12-21 00:34:59 +0400

    Mockito tests for DepartmentService

--- src/test/java/ru/skypro/course2/aliyev/EmployeeBookWithTests/constants/TestConstants.java
+++ src/test/java/ru/skypro/course2/aliyev/EmployeeBookWithTests/constants/TestConstants.java
@@ -9,7 +9,7 @@ public class TestConstants {
     public static final Employee EMPLOYEE1 = new Employee("Alexandr", "Kirshman", 90_000.0f, 2);
     public static final Employee EMPLOYEE2 = new Employee("Mary", "Ivanovich", 120_350.0f, 4);
     public static final Employee EMPLOYEE3 = new Employee("Ivan", "Fedorov", 100_000.0f, 1);
-    public static final Map<String, Employee> EMPLOYEES = new HashMap<String, Employee>(){{
+    public static final Map<String, Employee> EMPLOYEES = new HashMap<>(){{
        put("Ivan Fedorov", new Employee("Ivan", "Fedorov", 100_000.0f, 1));
        put("Petr Ivanov", new Employee("Petr", "Ivanov", 120_000.0f, 1));
        put("Maria Petrova", new Employee("Maria", "Petrova", 110_000.0f, 3));
@@ -24,13 +24,13 @@ public class TestConstants {
             .filter(employee -> employee.getDepartment() == DEPARTMENT_ID)
             .mapToDouble(Employee::getSalary)
             .sum();
-    public static final float MAX_BY_DEPARTMENT_ID_1 = (float) EMPLOYEES.values()
+    public static final float MAX_BY_DEPARTMENT_ID_1 = EMPLOYEES.values()
             .stream()
             .filter(employee -> employee.getDepartment() == DEPARTMENT_ID)
             .max(Comparator.comparingDouble(Employee::getSalary))
             .orElseThrow()
             .getSalary();
-    public static final float MIN_BY_DEPARTMENT_ID_1 = (float) EMPLOYEES.values()
+    public static final float MIN_BY_DEPARTMENT_ID_1 = EMPLOYEES.values()
             .stream()
             .filter(employee -> employee.getDepartment() == DEPARTMENT_ID)
             .min(Comparator.comparingDouble(Employee::getSalary))
